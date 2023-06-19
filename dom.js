ef05dbbddedfb01a2711d0a39660f53d25412419
amogh4319 <amogh4319@gmail.com>
2023-06-19 20:30:06 +0530

    second file

---
+++ dom.js
@@ -0,0 +1,17 @@
+//console.dir(document);
+//console.log(document.doctype);
+//console.log(document.title);
+//document.title=123;
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all[10]);
+//console.log(document.all);
+//document.all[10].textContent='Hello';
+//console.log(document.forms[10]);
+console.dir(document)
+//console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.style.borderBottom="solid 3px #000";
+header.style.borderBottom="solid 3px #000";
