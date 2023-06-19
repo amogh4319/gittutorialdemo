4048c8c40660fe67f107ff0f48f9b77fc085ba88
amogh4319 <amogh4319@gmail.com>
2023-06-19 21:05:36 +0530

    adding getElementByClassName

--- dom.js
+++ dom.js
@@ -8,10 +8,27 @@
 //console.log(document.all);
 //document.all[10].textContent='Hello';
 //console.log(document.forms[10]);
-console.dir(document)
+//console.dir(document)
 //console.log(document.getElementById('header-title'));
-var headerTitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
+//var headerTitle=document.getElementById('header-title');
+//var header=document.getElementById('main-header');
 //console.log(headerTitle);
 //headerTitle.style.borderBottom="solid 3px #000";
-header.style.borderBottom="solid 3px #000";
+//header.style.borderBottom="solid 3px #000";
+
+//GET ELEMENTBYCLASSNAME
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent="hello 2";
+items[1].style.fontWeight="bold";
+items[1].style.backgroundColor="yellow";
+for(var i=0;i<items.length;i++){
+
+    items[i].backgroundColor="grey";
+}
+items[2].style.backgroundColor="green";
+for(var i=0;i<items.length;i++){
+    
+    items[i].style.fontWeight="bold";
+}
