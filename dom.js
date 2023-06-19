e55380520b0b071430b263e123438ee63ae8a71f
amogh4319 <amogh4319@gmail.com>
2023-06-19 21:21:56 +0530

    added getByTagName

--- dom.js
+++ dom.js
@@ -17,18 +17,29 @@
 //header.style.borderBottom="solid 3px #000";
 
 //GET ELEMENTBYCLASSNAME
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent="hello 2";
-items[1].style.fontWeight="bold";
-items[1].style.backgroundColor="yellow";
-for(var i=0;i<items.length;i++){
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent="hello 2";
+// items[1].style.fontWeight="bold";
+// items[1].style.backgroundColor="yellow";
+// for(var i=0;i<items.length;i++){
 
-    items[i].backgroundColor="grey";
-}
-items[2].style.backgroundColor="green";
-for(var i=0;i<items.length;i++){
+//     items[i].backgroundColor="grey";
+// }
+// items[2].style.backgroundColor="green";
+// for(var i=0;i<items.length;i++){
     
-    items[i].style.fontWeight="bold";
+//     items[i].style.fontWeight="bold";
+// }
+
+//GET ELEMENTBYTAGNAME
+var li=document.getElementsByTagName('li');
+console.log(li[1]);
+li[1].textContent="hello 2";
+li[1].style.fontWeight="bold";
+li[1].style.backgroundColor="yellow";
+for(var i=0;i<li.length;i++){
+
+    li[i].style.backgroundColor="grey";
 }
